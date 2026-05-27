73642e07855286a5c0eebc5096d8f78d1d1bb9ab
ahryncyszyn <ahryncyszyn11@gmail.com>
2026-05-28 01:13:54 +0200

    added convnext_tiny model

--- src/models/factory.py
+++ src/models/factory.py
@@ -3,10 +3,7 @@ import torch.nn as nn
 from torchvision import models
 
 def create_model(model_name: str, num_classes: int):
-    if "vit" in model_name:
+    if model_name in ["vit_tiny_patch16_224", "convnext_tiny"]:
         return timm.create_model(model_name, pretrained = True, num_classes = num_classes)
-
-    ## MORE MODELS HERE AS ELIF
-
     else:
         raise ValueError(f"Architecture {model_name} is not supported.")
